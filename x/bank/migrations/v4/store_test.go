43b2ca5c15e9eac2f9de4932f48425bcfffcdccb
mmsqe <mavis@crypto.com>
2023-10-13 10:14:52 +0800

    fix test

--- x/bank/migrations/v4/store_test.go
+++ x/bank/migrations/v4/store_test.go
@@ -7,23 +7,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/cosmos/cosmos-sdk/x/bank"
-	"github.com/cosmos/cosmos-sdk/x/bank/exported"
 	v4 "github.com/cosmos/cosmos-sdk/x/bank/migrations/v4"
 	"github.com/cosmos/cosmos-sdk/x/bank/types"
 	"github.com/stretchr/testify/require"
 )
-
-type mockSubspace struct {
-	ps types.Params
-}
-
-func newMockSubspace(ps types.Params) mockSubspace {
-	return mockSubspace{ps: ps}
-}
-
-func (ms mockSubspace) GetParamSet(ctx sdk.Context, ps exported.ParamSet) {
-	*ps.(*types.Params) = ms.ps
-}
 
 func TestMigrate(t *testing.T) {
 	encCfg := moduletestutil.MakeTestEncodingConfig(bank.AppModuleBasic{})
@@ -34,11 +21,10 @@ func TestMigrate(t *testing.T) {
 	ctx := testutil.DefaultContext(storeKey, tKey)
 	store := ctx.KVStore(storeKey)
 
-	legacySubspace := newMockSubspace(types.DefaultParams())
-	require.NoError(t, v4.MigrateStore(ctx, storeKey, legacySubspace, cdc))
+	require.NoError(t, v4.MigrateStore(ctx, storeKey, types.DefaultParams(), cdc))
 
 	var res types.Params
 	bz := store.Get(v4.ParamsKey)
 	require.NoError(t, cdc.Unmarshal(bz, &res))
-	require.Equal(t, legacySubspace.ps, res)
+	require.Equal(t, types.DefaultParams(), res)
 }
